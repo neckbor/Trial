e701f2ee450914b821230e61c762650a55903780
neckbor <neckbor@mail.ru>
2025-06-13 03:56:38 +0400

    - add IFlightAggregator abstraction;

---
+++ DataWare/Application/FlightAggregation/IFlightAggregator.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using Domain.Models;
+using Domain.Shared;
+
+namespace Application.FlightAggregation;
+
+public interface IFlightAggregator
+{
+    Task<Result<List<BaseFlight>>> AggregateAsync(SearchRequest request, CancellationToken cancellationToken);
+}
