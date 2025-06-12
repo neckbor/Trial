9649b28abd83518dc4080d3a0b60bbc80549a631
neckbor <neckbor@mail.ru>
2025-06-13 01:24:26 +0400

    - add ApplicationErrors list;

---
+++ DataWare/Application/Errors/ApplicationErrors.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Shared;
+
+namespace Application.Errors;
+
+internal static class ApplicationErrors
+{
+    public static class General
+    {
+        public static readonly Error Unexpected = Error.Failure(
+            "General.Unexpected",
+            "Во время работы приложения произошла непредвиденная ошибка.");
+    }
+}
