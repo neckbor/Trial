59ca32aba91df63321548e95dbb061a50f236e17
neckbor <neckbor@mail.ru>
2025-06-12 19:58:26 +0400

    - add error/result models;

---
+++ DataWare/Domain/Shared/Error.cs
@@ -0,0 +1,58 @@
+﻿namespace Domain.Shared;
+
+public class Error : IEquatable<Error>
+{
+    public static readonly Error None = Error.Failure(string.Empty, string.Empty);
+    public static readonly Error NullValue = Error.Failure("Error.NullValue", "The specified result value is null.");
+
+    private Error(string code, string message, ErrorType type)
+    {
+        Code = code;
+        Message = message;
+        Type = type;
+    }
+
+    public string Code { get; }
+
+    public string Message { get; }
+
+    public ErrorType Type { get; }
+
+    public static Error Failure(string code, string message) => new Error(code, message, ErrorType.Failure);
+    public static Error NotFound(string code, string message) => new Error(code, message, ErrorType.NotFound);
+    public static Error Conflict(string code, string message) => new Error(code, message, ErrorType.Conflict);
+    public static Error Validation(string code, string message) => new Error(code, message, ErrorType.Validation);
+    public static Error Unauthorized(string code, string message) => new Error(code, message, ErrorType.Unauthorized);
+    public static Error Forbidden(string code, string message) => new Error(code, message, ErrorType.Forbidden);
+
+    public static bool operator ==(Error? a, Error? b)
+    {
+        if (a is null && b is null)
+        {
+            return true;
+        }
+
+        if (a is null || b is null)
+        {
+            return false;
+        }
+
+        return a.Equals(b);
+    }
+    public static bool operator !=(Error? a, Error? b) => !(a == b);
+
+    public virtual bool Equals(Error? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        return Code == other.Code && Message == other.Message;
+    }
+    public override bool Equals(object? obj) => obj is Error error && Equals(error);
+
+    public static implicit operator string(Error error) => error.Code;
+    public override int GetHashCode() => HashCode.Combine(Code, Message);
+    public override string ToString() => Code;
+}
---
+++ DataWare/Domain/Shared/Result.cs
@@ -0,0 +1,39 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Domain.Shared;
+
+public class Result
+{
+    protected internal Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None)
+        {
+            throw new InvalidOperationException("Result is successed but error is not none");
+        }
+
+        if (!isSuccess && error == Error.None)
+        {
+            throw new InvalidOperationException("Result is failure but error is none");
+        }
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; }
+
+    public static Result Success() => new(true, Error.None);
+    public static Result<TValue> Success<TValue>(TValue value) =>
+        new(value, true, Error.None);
+
+    public static Result Failure(Error error) => new(false, error);
+    public static Result<TValue> Failure<TValue>(Error error) =>
+        new(default, false, error);
+
+    public static Result<TValue> Create<TValue>(TValue? value) =>
+        value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
+}
