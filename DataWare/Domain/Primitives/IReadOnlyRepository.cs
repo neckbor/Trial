24ef3fb3aab10e8b08c535c4b59b8b119eaf26fa
neckbor <neckbor@mail.ru>
2025-06-13 00:56:25 +0400

    - add repository abstractions;

---
+++ DataWare/Domain/Primitives/IReadOnlyRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.Primitives;
+
+public interface IReadOnlyRepository<TEntity, in TKey>
+    where TEntity : Entity<TKey>
+    where TKey : IComparable
+{
+    Task<TResult> FirstOrDefaultAsync<TResult>(
+        Expression<Func<TEntity, bool>> condition,
+        params Expression<Func<TEntity, object>>[] includedPaths);
+}
---
+++ DataWare/Domain/Primitives/IRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Primitives;
+
+public interface IRepository<TEntity, in TKey> : IReadOnlyRepository<TEntity, TKey>
+    where TEntity : Entity<TKey>
+    where TKey : IComparable
+{
+}
