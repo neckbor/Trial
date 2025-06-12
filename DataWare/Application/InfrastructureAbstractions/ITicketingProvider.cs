300742e0a6805f579ec6b69d3ac68961a2091697
neckbor <neckbor@mail.ru>
2025-06-13 03:49:04 +0400

    - add ITicketProvider abstraction;

---
+++ DataWare/Application/InfrastructureAbstractions/ITicketingProvider.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Domain.Entities.Dictionaries;
+using Domain.Models;
+using Domain.Shared;
+
+namespace Application.InfrastructureAbstractions;
+
+public interface ITicketingProvider
+{
+    TicketingProvider Provider { get; }
+    Task<Result<List<BaseFlight>>> SearchAsync(SearchRequest request);
+}
