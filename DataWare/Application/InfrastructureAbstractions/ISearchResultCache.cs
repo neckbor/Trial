5656b8fb3e1927d209fce45ed973fcea084aa5bd
neckbor <neckbor@mail.ru>
2025-06-13 04:03:45 +0400

    - add ISearchResult abstraction;

---
+++ DataWare/Application/InfrastructureAbstractions/ISearchResultCache.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models;
+
+namespace Application.InfrastructureAbstractions;
+
+public interface ISearchResultCache
+{
+    Task SaveAsync(Guid searchId, List<BaseFlight> flights);
+    Task<List<BaseFlight>> GetAsync(Guid searchId);
+}
