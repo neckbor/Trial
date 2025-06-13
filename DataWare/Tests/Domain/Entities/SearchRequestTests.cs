bdd1f66f94544a46a923a8e131702d26d279c4da
neckbor <neckbor@mail.ru>
2025-06-13 13:53:02 +0400

    - add Tests project; - add Domain.Entities.SearchRequest tests;

---
+++ DataWare/Tests/Domain/Entities/SearchRequestTests.cs
@@ -0,0 +1,81 @@
+﻿using Domain.Entities;
+using Domain.Entities.Dictionaries;
+using Domain.Errors;
+using FluentAssertions;
+
+namespace Tests.Domain.Entities;
+
+public class SearchRequestTests
+{
+    [Fact]
+    public void Create_Should_ReturnSuccess_WhenValidData()
+    {
+        // Arrange
+        var clientId = "";
+        var from = Airport.Create(1, "JFK", "JFK");
+        var to = Airport.Create(2, "LAX", "LAX");
+        var departureDate = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(10));
+        var passengerCount = 1;
+
+        // Act
+        var result = SearchRequest.Create(clientId, from, to, departureDate, passengerCount);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().NotBe(null);
+    }
+
+    [Fact]
+    public void Create_Should_ReturnError_WhenInvalidDate()
+    {
+        // Arrange
+        var clientId = "";
+        var from = Airport.Create(1, "JFK", "JFK");
+        var to = Airport.Create(2, "LAX", "LAX");
+        var departureDate = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(-1));
+        var passengerCount = 1;
+
+        // Act
+        var result = SearchRequest.Create(clientId, from, to, departureDate, passengerCount);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(DomainErrors.SearchRequest.InvalidDepartureDate);
+    }
+
+    [Fact]
+    public void Create_Should_ReturnError_WhenSameAirports()
+    {
+        // Arrange
+        var clientId = "";
+        var from = Airport.Create(1, "JFK", "JFK");
+        var to = from;
+        var departureDate = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(10));
+        var passengerCount = 1;
+
+        // Act
+        var result = SearchRequest.Create(clientId, from, to, departureDate, passengerCount);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(DomainErrors.SearchRequest.SameAirports);
+    }
+
+    [Fact]
+    public void Create_Should_ReturnError_WhenInvalidPassengerCount()
+    {
+        // Arrange
+        var clientId = "";
+        var from = Airport.Create(1, "JFK", "JFK");
+        var to = Airport.Create(2, "LAX", "LAX"); ;
+        var departureDate = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(10));
+        var passengerCount = 0;
+
+        // Act
+        var result = SearchRequest.Create(clientId, from, to, departureDate, passengerCount);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(DomainErrors.SearchRequest.InvalidPassengerCount);
+    }
+}
