9fd0dc16f3bcbdf8a4d8e948710d8185d6b6d1ca
neckbor <neckbor@mail.ru>
2025-06-16 01:40:08 +0400

    - temporary db context factory;

---
+++ DataWare/DataAccess/TemporaryDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccess;
+
+internal class TemporaryDbContextFactory : IDesignTimeDbContextFactory<DataWareDbContext>
+{
+    public DataWareDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "db-connection.json"))
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<DataWareDbContext>()
+            .UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+            .EnableDetailedErrors()
+            .EnableSensitiveDataLogging()
+            .UseSnakeCaseNamingConvention();
+
+        return new DataWareDbContext(optionsBuilder.Options);
+    }
+}
