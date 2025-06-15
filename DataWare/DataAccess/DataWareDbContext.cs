6fd239a92ece7f3edb82cf6430f174d1791e25c1
neckbor <neckbor@mail.ru>
2025-06-16 01:39:49 +0400

    - add db context;

---
+++ DataWare/DataAccess/DataWareDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+internal class DataWareDbContext : DbContext
+{
+    public DataWareDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
+}
