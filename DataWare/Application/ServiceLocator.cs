e814a7bb167aeb0d3d3999566521b8473976909f
neckbor <neckbor@mail.ru>
2025-06-16 02:17:21 +0400

    - application layer service locator;

---
+++ DataWare/Application/ServiceLocator.cs
@@ -0,0 +1,22 @@
+﻿using Application.Booking;
+using Application.Dictionaries.Airlines;
+using Application.Dictionaries.Airports;
+using Application.Dictionaries.Countries;
+using Application.FlightAggregation;
+using Application.FlightSearch;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class ServiceLocator
+{
+    public static void RegisterApplicationServices(IServiceCollection services)
+    {
+        services.AddScoped<IFlightSearchService, FlightSearchService>();
+        services.AddScoped<IFlightAggregator, FlightAggregator>();
+        services.AddScoped<IBookingService, BookingService>();
+        services.AddScoped<IAirlineService, AirlineService>();
+        services.AddScoped<IAirportService, AirportService>();
+        services.AddScoped<ICountryService, CountryService>();
+    }
+}
