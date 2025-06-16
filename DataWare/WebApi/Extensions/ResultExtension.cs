42c2de7d79758b2a464988071d70672489694631
neckbor <neckbor@mail.ru>
2025-06-16 04:24:55 +0400

    - add result extension that maps Error to IResult;

---
+++ DataWare/WebApi/Extensions/ResultExtension.cs
@@ -0,0 +1,56 @@
+﻿using Domain.Shared;
+
+namespace WebApi.Extensions;
+
+internal static class ResultExtension
+{
+    public static IResult ToProblem(this Result result)
+    {
+        if (result.IsSuccess)
+        {
+            throw new InvalidOperationException("Result is succeeded but trying to build the Problem API response.");
+        }
+
+        return Results.Problem(
+            statusCode: GetStatusCode(result.Error.Type),
+            title: GetTitle(result.Error.Type),
+            type: GetType(result.Error.Type),
+            extensions: new Dictionary<string, object?>
+            {
+                { "errors", new[] { result.Error } }
+            });
+
+        static int GetStatusCode(ErrorType errorType) =>
+            errorType switch
+            {
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
+                ErrorType.Forbidden => StatusCodes.Status403Forbidden,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+        static string GetTitle(ErrorType errorType) =>
+            errorType switch
+            {
+                ErrorType.NotFound => "Not Found",
+                ErrorType.Conflict => "Conflict",
+                ErrorType.Validation => "Bad Request",
+                ErrorType.Unauthorized => "Unauthorized",
+                ErrorType.Forbidden => "Forbidden",
+                _ => "Server Failure"
+            };
+
+        static string GetType(ErrorType errorType) =>
+            errorType switch
+            {
+                ErrorType.NotFound => "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+                ErrorType.Conflict => "https://tools.ietf.org/html/rfc7231#section-6.5.8",
+                ErrorType.Validation => "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+                ErrorType.Unauthorized => "https://tools.ietf.org/html/rfc7235#section-3.1",
+                ErrorType.Forbidden => "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.3",
+                _ => "https://tools.ietf.org/html/rfc7231#section-6.6.1"
+            };
+    }
+}
