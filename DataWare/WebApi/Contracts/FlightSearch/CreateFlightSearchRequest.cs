b2609df39dd29685c4f3ea82f278239ee3057d06
neckbor <neckbor@mail.ru>
2025-06-16 04:59:34 +0400

    - add flight search controller;

---
+++ DataWare/WebApi/Contracts/FlightSearch/CreateFlightSearchRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Contracts.FlightSearch;
+
+public class CreateFlightSearchRequest
+{
+    public DateOnly DepartureDate { get; set; }
+    public string FromIATA { get; set; }
+    public string ToIATA { get; set; }
+    public int PassengerCount { get; set; }
+}
---
+++ DataWare/WebApi/Controllers/FlightSearchController.cs
@@ -0,0 +1,70 @@
+﻿using Application.FlightSearch;
+using Application.FlightSearch.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Contracts.FlightSearch;
+using WebApi.Extensions;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightSearchController : ControllerBase
+    {
+        private readonly IFlightSearchService _flightSearchService;
+        private readonly LinkGenerator _linkGenerator;
+
+        public FlightSearchController(IFlightSearchService flightSearchService, LinkGenerator linkGenerator)
+        {
+            _flightSearchService = flightSearchService;
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpPost]
+        public async Task<IResult> PostSearchAsync(CreateFlightSearchRequest request)
+        {
+            var command = new StartSearchCommand(Guid.NewGuid().ToString(), request.DepartureDate, request.FromIATA, request.ToIATA, request.PassengerCount);
+
+            var result = await _flightSearchService.CreateSearchRequestAsync(command);
+
+            if (result.IsFailure)
+            {
+                return result.ToProblem();
+            }
+
+            Guid searchRequestId = result.Value;
+
+            var location = _linkGenerator.GetPathByAction(
+                HttpContext,
+                action: nameof(GetSearchResultsAsync),
+                controller: "FlightSearch",
+                values: new { Id = searchRequestId });
+
+            return Results.Created(location, searchRequestId);
+        }
+
+        [HttpGet("{requestId:guid}")]
+        [ActionName(nameof(GetSearchResultsAsync))]
+        public async Task<IResult> GetSearchResultsAsync(Guid requestId, 
+            [FromQuery] int maxTranfers,
+            [FromQuery] decimal? maxPrice,  
+            [FromQuery] List<string>? airlineCodes, 
+            [FromQuery] FlightSearchResultSortOption? sortOption, 
+            [FromQuery] bool? sortDescending)
+        {
+            var query = new GetSearchResultsQuery(requestId, new SearchResultFilterOptions
+            {
+                AirlineCodes = airlineCodes,
+                MaxPrice = maxPrice,
+                MaxTransfers = maxTranfers,
+                SortOption = sortOption ?? FlightSearchResultSortOption.Price,
+                SortDescending = sortDescending ?? false
+            });
+
+            var result = await _flightSearchService.GetSearchResultAsync(query);
+
+            return result.IsSuccess ? Results.Ok(result.Value) 
+                                    : result.ToProblem();
+        }
+    }
+}
